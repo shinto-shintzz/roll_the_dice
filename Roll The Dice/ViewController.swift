969b22fdb281f098d1dfc27092f712a2aa207d53
Shinto Joseph <shintojoseph@Shintos-MacBook-Air.local>
2017-12-01 23:55:48 +0530

    first Commit

--- Roll The Dice/ViewController.swift
+++ Roll The Dice/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ramdomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func rollButtonTapped(_ sender: Any) {
+        
+        updateDiceImages()
+        
     }
-
+    
+    func updateDiceImages(){
+        
+        ramdomDiceIndex1 = Int(1+(arc4random_uniform(6)))
+        randomDiceIndex2 = Int(1+(arc4random_uniform(6)))
+        
+        diceImageView1.image = UIImage(named: "dice\(ramdomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 
 }
 
